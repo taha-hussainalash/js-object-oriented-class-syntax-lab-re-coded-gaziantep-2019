3ced05e54c1bac24ef60e6dfdfcfd341d26f1ec8
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-25 13:34:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class BoardMember{
     return "You will see great things from Scuber."
   }
   sayHi(){
-    return `Hi, my name is + this.name . I am from ${this.homeState}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
 
